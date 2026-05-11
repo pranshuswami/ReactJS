085bc519598ece0bddedf5782f98a037708cc168
Pranshu Swami <pranshuswami185@gmail.com>
2026-05-11 22:02:31 +0530

    REACTJS2026 done

---
+++ REACTJS/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+const App = () => {
+  return (
+   <>
+   <div id='parent'>
+      <h1 id='child1'>App</h1>
+      <h3 id='child2'>hello</h3>
+      </div>
+  <div id="chacha"></div>
+  </>
+  )
+}
+
+export default App
